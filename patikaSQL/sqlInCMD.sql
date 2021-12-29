ae809bb7644af8ac09973e231eaae77cb9853215
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-12-29 08:59:05 -0600

    sqlCMD

---
+++ patikaSQL/sqlInCMD.sql
@@ -0,0 +1,17 @@
+1- PSQL ile PostgreSQL'e bağlanmak.
+psql -U <kullanıcı_adı>
+2- Kullanıcıya ait şifreyi girdikten sonra varsayılan veritabanı postgres'e bağlanıyor.
+postgres=#
+3- Bulunan veritabanlarını listelemek için:
+\l veya \list
+4- Bizim örneğimizde dvdrental veritabanına bağlanacağız.
+\c dvdrental veya \connect dvdrental
+5- Bağlanılan dvdrental veritabanında bulunan tabloları listelemek için:
+\dt
+6- Herhangi bir tablonun sütunlarını ve tablo detaylarını görmek için:
+\d <tablo_adı>
+
+
+1-\dt--> shows tables
+2-\d actor --> shows columns at table
+3-\c dvdrental --> connect to database
